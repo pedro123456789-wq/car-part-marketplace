0df67e0d97768adeffac0e10630e3c20b7564475
ebharathi <elayabharathi10102002@gmail.com>
2024-10-17 00:48:08 +0530

    Dropdown change

--- src/app/components/DropdownInput.tsx
+++ src/app/components/DropdownInput.tsx
@@ -1,5 +1,5 @@
-import React, { useState, ChangeEvent, useRef, useEffect } from "react";
-
+import React, { useState, useRef, useEffect } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 type DropdownProps = {
   options: string[];
   placeholder: string;
@@ -37,33 +37,47 @@ const DropDownInput: React.FC<DropdownProps> = ({
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
-
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   return (
-    <div ref={dropdownRef} className="min-w-32 text-center">
-      <p className="text-sm border-2 border-gray-200 p-2 rounded-lg bg-white">{selectedText || "Select a value"}</p>
+    <div ref={dropdownRef} className="relative min-w-36  text-center">
+      {/* Toggle dropdown on click */}
+      <p
+        className="text-sm flex justify-center items-center border-2 border-gray-200 py-2 rounded-lg bg-white cursor-pointer"
+        onClick={() => setIsDropdownOpen((prev) => !prev)}
+      >
 
-      <div className="w-full border-2 border-gray-200 rounded-lg shadow-lg z-10">
-        <ul className="flex flex-col bg-base-100 max-h-60 overflow-y-auto rounded-lg">
-          {options.length > 0 ? (
-            options.map((option) => (
-              <li
-                key={option}
-                onClick={() => handleOptionClick(option)}
-                className="cursor-pointer hover:bg-gray-300 p-2 rounded-lg text-sm"
-              >
-                {option}
-              </li>
-            ))
-          ) : (
-            <li className="p-2 text-gray-500">No matches</li>
-          )}
-        </ul>
-      </div>
+        <span>{selectedText || "Select a value"}</span>
+        {isDropdownOpen ? (
+          <IoIosArrowUp className="w-4 h-4 ml-1 text-gray-500" />
+        ) : (
+          <IoIosArrowDown className="w-4 h-4 ml-1 text-gray-500" />
+        )}
+      </p>
+
+      {/* Dropdown options, visible only when isDropdownOpen is true */}
+      {isDropdownOpen && (
+        <div className="absolute w-full border-2 border-gray-200 rounded-lg shadow-lg z-10 bg-base-100 mt-1">
+          <ul className="flex flex-col max-h-60 overflow-y-auto rounded-lg">
+            {options.length > 0 ? (
+              options.map((option) => (
+                <li
+                  key={option}
+                  onClick={() => handleOptionClick(option)}
+                  className="cursor-pointer hover:bg-gray-300 p-2 rounded-sm text-sm"
+                >
+                  {option}
+                </li>
+              ))
+            ) : (
+              <li className="p-2 text-gray-500">No matches</li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
